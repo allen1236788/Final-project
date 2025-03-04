8c10cba3a9061f78f8fad0d990d62d5e2a659996
Xalleniee <charllencalvo@gmail.com>
2025-03-04 22:11:02 +0800

    Final Source Code Renew.java

--- Renew.java
+++ Renew.java
@@ -5,23 +5,16 @@ import java.awt.Font;
 public class Renew{
     public Renew(String lname, String fname, String mname, String suffix,
     String bday, String age, String sex, String address, String contact){
-
-
-
-
+     
         JFrame frame=new JFrame("Renewal");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(630,700);
         frame.setLayout(null);
 
-
-
-
         JLabel VehicleInfo = new JLabel("Vehicle Information");
         VehicleInfo.setBounds(50, 15, 200,100);
         VehicleInfo.setFont(new Font("Arial", Font.BOLD, 17));
         frame.add(VehicleInfo);
-
 
         JLabel labelKind=new JLabel("Kind of Vehicle: ");
         labelKind.setBounds(50,110,150,30);
@@ -35,11 +28,9 @@ public class Renew{
         VehicleModel.setBounds(390,190,150,30);
         frame.add(VehicleModel);
 
-
         JLabel VehicleDate=new JLabel("Vehicle Model Year: ");
         VehicleDate.setBounds(50,190,150,30);
         frame.add(VehicleDate);
-
 
         JLabel VehicleClassification=new JLabel("Vehicle Classification:");
         VehicleClassification.setBounds(280,110,300,30);
@@ -53,41 +44,30 @@ public class Renew{
         InsuranceCompanyName.setBounds(50,190,200,200);
         frame.add(InsuranceCompanyName);
 
-
         JLabel OldCertificationNum=new JLabel("Old Certification Number: ");
         OldCertificationNum.setBounds(50,290,200,200);
         frame.add(OldCertificationNum);
 
-
-
-
-       
-       
         JTextField textFieldOldCertificationNum=new JTextField();
         textFieldOldCertificationNum.setBounds(200,370,360,40);
         frame.add(textFieldOldCertificationNum);
-
 
         JTextField textFieldInsuranceCompanyName = new JTextField();
         textFieldInsuranceCompanyName.setBounds(50,310,200,40);  
         frame.add(textFieldInsuranceCompanyName);
 
-
         JTextField textFieldDateReleased = new JTextField();
         textFieldDateReleased.setBounds(50,230,120,40);  
         frame.add(textFieldDateReleased);
-
 
         JTextField textFieldVehiclePlate = new JTextField();
         textFieldVehiclePlate.setBounds(360,310,200,40);  
         frame.add(textFieldVehiclePlate);
 
-
         String[]Classification={"","PRIVATE","GOVERNMENT","DIPLOMATIC"};
         JComboBox<String>vehicleClass=new JComboBox<>(Classification);
         vehicleClass.setBounds(280,140,280,40);
         frame.add(vehicleClass);
-
 
         String[]Kind={"","SEDAN","MOTORCYCLE","TRUCK","SUV","VAN"};
         JComboBox<String>vehicleKind=new JComboBox<>(Kind);
@@ -98,28 +78,13 @@ public class Renew{
         textFieldVehicleMake.setBounds(195,230,170,40);  
         frame.add(textFieldVehicleMake);
 
-
         JTextField textFieldVehicleModel = new JTextField();
         textFieldVehicleModel.setBounds(390,230,170,40);  
         frame.add(textFieldVehicleModel);
 
-
-
-
-
-
-
-
         JButton buttonS= new JButton("RENEW");
         buttonS.setBounds(185,500,250,80);
         frame.add(buttonS);
-
-
-
-
-
-
-
 
         buttonS.addActionListener(new ActionListener(){
             @Override
@@ -143,9 +108,6 @@ public class Renew{
                 String License = textFieldVehiclePlate.getText().toUpperCase();
                 String OldCertNum = textFieldOldCertificationNum.getText();
 
-
-
-
                 if(Make.isEmpty()|| Kind.equals("")|| Model.isEmpty() || DateReleased.isEmpty() || Classification.equals("") || InsuranceCompany.isEmpty()|| License.isEmpty()|| OldCertNum.isEmpty()){
                     JOptionPane.showMessageDialog(null, "Answer all the fields", "Empty Field", JOptionPane.ERROR_MESSAGE);
                
@@ -167,13 +129,9 @@ public class Renew{
                                     Contact, Kind, Make, Model, DateReleased, Classification, InsuranceCompany,
                                     License, OldCertNum);
                                 }
-
-
-
-
                             }catch (Exception ex){
                                 throw new Exception("Old Certificate Number and Vehicle Model Year should not contain letters.");
-                                }
+                             }
                         }
                     }
             }catch (NumberFormatException ex) {
@@ -186,4 +144,3 @@ public class Renew{
         frame.setVisible(true);
         }  
             }
-
